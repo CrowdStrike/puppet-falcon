7c61baa9bd0fd53cdbb4c416b907cec788395461
ffalor <ffalorjr@outlook.com>
2022-03-01 17:05:50 +0000

    add puppet strings

--- lib/puppet/functions/falcon/sensor_download_info.rb
+++ lib/puppet/functions/falcon/sensor_download_info.rb
@@ -3,6 +3,25 @@ require_relative '../../puppet_x/helper'
 
 # Get sensor info like install package SHA and version
 Puppet::Functions.create_function(:'falcon::sensor_download_info') do
+  # @param client_id the client id used to authenticate with the Falcon API
+  # @param client_secret the client secret used to authenticate with the Falcon API
+  # @param options used to determine how download information is retrieved
+  #
+  #   - `version` the version of the sensor to use
+  #   - `falcon_cloud` the name of the cloud to use
+  #   - `update_policy` the update policy to use
+  #   - `sensor_tmp_dir` the temporary directory to use
+  # @return [Hash] download information about the sensor
+  #
+  #   - `sha256` the SHA256 checksum of the sensor package
+  #   - `version` the version of the sensor package
+  #   - `os_name` the name of the operating system the sensor is for
+  #   - `file_path` the fully qualified file path to download the sensor package to
+  #   - `bearer_token` the bearer token used to authenticate with the Falcon API
+  #   - `platform_name` the name of the platform the sensor is for
+  # @example Calling the function
+  #   falcon::sensor_download_info('client_id', 'client_secret', { 'falcon_cloud' => 'api.crowdstrike.com'})
+  #
   dispatch :sensor_download_info do
     param 'Sensitive', :client_id
     param 'Sensitive', :client_secret
@@ -14,7 +33,7 @@ Puppet::Functions.create_function(:'falcon::sensor_download_info') do
     scope = closure_scope
 
     platform_name = platform(scope)
-    os_name = scope['facts']['os']['name']
+    os_name = platform_name.casecmp('mac').zero? ? 'macOS' : scope['facts']['os']['name']
 
     falcon_api = FalconApi.new(falcon_cloud: options['falcon_cloud'], client_id: client_id, client_secret: client_secret)
 
--- lib/puppet/puppet_x/helper.rb
+++ lib/puppet/puppet_x/helper.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'json'
 require 'cgi'
 
+# Build the query string used to filter sensor installers
 def build_sensor_installer_query(platform_name:, os_name: nil, version: nil)
   query = "platform:'#{platform_name.downcase}'"
 
@@ -17,6 +18,7 @@ def build_sensor_installer_query(platform_name:, os_name: nil, version: nil)
   query
 end
 
+# Grabs the platform name that the falcon api expects
 def platform(scope)
   if scope['facts']['os']['macosx']
     return 'Mac'
