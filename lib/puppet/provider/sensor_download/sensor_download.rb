27245f27ae7fc24382e3254f15ee51c61c620c4d
ffalor <ffalorjr@outlook.com>
2022-07-07 14:57:58 -0500

    magic

--- lib/puppet/provider/sensor_download/sensor_download.rb
+++ lib/puppet/provider/sensor_download/sensor_download.rb
@@ -17,11 +17,7 @@ Puppet::Type.type(:sensor_download).provide(:default) do
   end
 
   def exists?
-    falcon_version = if Facter.value('falcon').nil?
-                       :absent
-                     else
-                       Facter.value('falcon').fetch('version', :absent)
-                     end
+    falcon_version = Facter.value('falcon')&.fetch('version') || :absent
 
     installed = [:absent, :purged, :undef, nil].include?(falcon_version) ? false : true
 
