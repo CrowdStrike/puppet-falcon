98b132ea2004d26bf2a2f262e97d4b771bc99d20
ffalor <35144141+ffalor@users.noreply.github.com>
2022-09-20 19:49:53 +0000

    fix falcon fact to work on windows

--- lib/facter/falcon.rb
+++ lib/facter/falcon.rb
@@ -1,26 +1,28 @@
 Facter.add(:falcon, type: :aggregate) do
   chunk(:version) do
-    kernel = Facter.value('kernel')
+    begin
+      kernel = Facter.value('kernel')
 
-    pkg_name = if kernel == 'Linux'
-                 'falcon-sensor'
-               elsif kernel == 'windows'
-                 'CrowdStrike Windows Sensor'
-               else
-                 'falcon'
-               end
+      pkg_name = if kernel == 'Linux'
+                  'falcon-sensor'
+                elsif kernel == 'windows'
+                  'CrowdStrike Windows Sensor'
+                else
+                  'falcon'
+                end
 
-    pkg_ensure = Puppet::Resource.indirection.find("package/#{pkg_name}").to_hash[:ensure]
+      pkg_ensure = Puppet::Resource.indirection.find("package/#{pkg_name}").to_hash[:ensure]
 
-    Puppet.debug("#{pkg_name} returned ensure: #{pkg_ensure}")
+      Puppet.debug("#{pkg_name} returned ensure: #{pkg_ensure}")
 
-    if [:purged, :absent, :undef].include?(pkg_ensure)
-      pkg_ensure = :absent
+      if [:purged, :absent, :undef].include?(pkg_ensure)
+        pkg_ensure = :absent
+      end
+
+      { version: pkg_ensure }
+    rescue => exception
+      Puppet.debug("#{pkg_name} returned exception: #{exception}")
+      { version: :absent }
     end
-
-    { version: pkg_ensure }
-  rescue => exception
-    Puppet.debug("#{pkg_name} returned exception: #{exception}")
-    { version: :absent }
   end
 end
