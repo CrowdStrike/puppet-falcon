a7825b16cdd61ed53f24212e7c43282896b7b8c5
ffalor <35144141+ffalor@users.noreply.github.com>
2022-10-07 21:28:06 +0000

    add `aid` to fact `falcon`

--- lib/facter/falcon.rb
+++ lib/facter/falcon.rb
@@ -26,4 +26,45 @@ Facter.add(:falcon, type: :aggregate) do
       { version: :absent }
     end
   end
+
+  chunk(:aid) do
+    begin
+      kernel = Facter.value('kernel')
+
+      next unless [ 'Linux', 'windows' ].include?(kernel)
+
+      if kernel == 'Linux'
+        aid = Facter::Core::Execution.execute('/opt/CrofwdStrike/falconctl -g --aid', { on_fail: :raise })
+
+        if aid.nil? || aid.empty?
+          next
+        end
+
+        aid = aid.gsub(%r{aid="|".}, '')
+      elsif kernel == 'windows'
+        require 'win32/registry'
+
+        ['SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-7058-48c9-a204-725362b67639}\Default', 'SYSTEM\CurrentControlSet\Services\CSAgent\Sim'].each do |reg_path|
+          begin
+            Win32::Registry::HKEY_LOCAL_MACHINE.open(reg_path) do |reg|
+              aid = reg.read('AG')[-1].unpack('H*')[0]
+              break
+            end
+          rescue Win32::Registry::Error => exception
+            Puppet.debug("Registry exception: #{exception}")
+            next
+          end
+        end
+      end
+
+      if aid.nil? || aid.empty? || aid.include?('aid is not set.')
+        aid = :unset
+      end
+
+      { aid: aid }
+    rescue => exception
+      Puppet.debug("Unable to retrieve AID: #{exception}")
+      next
+    end
+  end
 end
