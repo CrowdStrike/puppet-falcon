1537549b446b7f75db22c7913a607ae84118847d
ffalor <35144141+ffalor@users.noreply.github.com>
2022-10-19 19:01:37 +0000

    fix syntax errors

--- spec/acceptance/falcon_install_spec.rb
+++ spec/acceptance/falcon_install_spec.rb
@@ -162,23 +162,22 @@ describe 'install falcon' do
         MANIFEST
 
         command('/opt/CrowdStrike/falconctl -sf --tags=removetag') do
+          it 'applies idempotently' do
+            apply_manifest(manifest, { catch_failures: true, debug: true })
+            apply_manifest(manifest, { catch_changes: true, debug: true })
+          end
 
-        it 'applies idempotently' do
-          apply_manifest(manifest, { catch_failures: true, debug: true })
-          apply_manifest(manifest, { catch_changes: true, debug: true })
+          describe command('/opt/CrowdStrike/falconctl -g --tags') do
+            its(:stdout) { is_expected.to match(%r{tags=tag1,tag2}) }
+          end
+
+          describe package('falcon-sensor') do
+            it { is_expected.to be_installed }
+          end
         end
 
-        describe command('/opt/CrowdStrike/falconctl -g --tags') do
-          its(:stdout) { is_expected.to match(%r{tags=tag1,tag2}) }
-        end
-
-        describe package('falcon-sensor') do
-          it { is_expected.to be_installed }
-        end
-      end
-
-      context 'purge tags' do
-        manifest = <<-MANIFEST
+        context 'purge tags' do
+          manifest = <<-MANIFEST
           class { 'falcon':
             falcon_cloud => 'api.us-2.crowdstrike.com',
             client_id => Sensitive('#{ENV['CLIENT_ID']}'),
@@ -189,19 +188,20 @@ describe 'install falcon' do
           }
         MANIFEST
 
-        command('/opt/CrowdStrike/falconctl -sf --tags=ci,testing') do
+          command('/opt/CrowdStrike/falconctl -sf --tags=ci,testing') do
+            it 'applies idempotently' do
+              apply_manifest(manifest, { catch_failures: true, debug: true })
+              apply_manifest(manifest, { catch_changes: true, debug: true })
+            end
 
-        it 'applies idempotently' do
-          apply_manifest(manifest, { catch_failures: true, debug: true })
-          apply_manifest(manifest, { catch_changes: true, debug: true })
-        end
+            describe command('/opt/CrowdStrike/falconctl -g --tags') do
+              its(:stdout) { is_expected.to match(%r{tags are not set}) }
+            end
 
-        describe command('/opt/CrowdStrike/falconctl -g --tags') do
-          its(:stdout) { is_expected.to match(%r{tags are not set}) }
-        end
-
-        describe package('falcon-sensor') do
-          it { is_expected.to be_installed }
+            describe package('falcon-sensor') do
+              it { is_expected.to be_installed }
+            end
+          end
         end
       end
     end
